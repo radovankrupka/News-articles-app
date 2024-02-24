86191e79762c1efeb1a0eba73bc3878cfacb3cc1
radovankrupka <rkrupka01@gmail.com>
2024-02-24 14:26:55 +0100

    update of configuration and dependecies, for war deployment to local tomcat

--- src/main/java/com/dbtech/finalapp/CrudappApplication.java
+++ src/main/java/com/dbtech/finalapp/CrudappApplication.java
@@ -2,9 +2,11 @@ package com.dbtech.finalapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class CrudappApplication {
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+public class CrudappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudappApplication.class, args);
